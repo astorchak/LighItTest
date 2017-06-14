f576ffef9ed6a5a88baf84c378923846336684d5
Anton <stor.anton.dev@gmail.com>
2017-06-14 13:37:24 +0300

    removed redundant imports

--- app/src/main/java/com/example/user1/lightittest/MainActivity.java
+++ app/src/main/java/com/example/user1/lightittest/MainActivity.java
@@ -3,23 +3,15 @@ package com.example.user1.lightittest;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.user1.lightittest.Model.Product;
-import com.koushikdutta.ion.Ion;
 
 import java.util.List;
 
-import butterknife.BindView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
